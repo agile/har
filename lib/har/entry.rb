36e9519a0ac56ece21bca3cb68f12dd416b7fe3c
Mike Vincent <mike@collective.com>
2013-12-07 02:18:53 -0600

    Add Entry conveniences and specs
    * Delegate methods from request and response
    * successful? predicate based on response.is_error?
    * response_code alias for response.status

---
+++ lib/har/entry.rb
@@ -0,0 +1,27 @@
+require 'forwardable'
+module HAR
+  class Entry < SchemaType
+    extend Forwardable
+
+    def_delegators :response, *[:error?, :client_error?, :server_error?,
+                                :connection_error?, :redirect?, :content_type?,
+                                :html?, :image?, :javascript?, :css?, :flash?,
+                                :video?, :content_type?, :has_content?,
+                                :have_content?, :matches_content?,
+                                :match_content?,  :redirected_to]
+
+    def_delegators :request, *[:url]
+    def initialize(input)
+      super(input)
+    end
+
+    def response_code
+      response.status
+    end
+
+    def successful?
+      !response.is_error?
+    end
+
+  end
+end
--- spec/har/entry_spec.rb
+++ spec/har/entry_spec.rb
@@ -12,5 +12,49 @@ module HAR
       entry.response.should be_kind_of(Response)
     end
 
+    it "should be successful if response has no error" do
+      entry.response.stub(:is_error?).and_return(false)
+      entry.should be_successful
+    end
+
+    it "should not be successful if response has an error" do
+      entry.response.stub(:is_error?).and_return(true)
+      entry.should_not be_successful
+    end
+
+    context "delegated methods from response" do
+      it "should include :response_code as an alias for response.status" do
+        entry.response_code.should == entry.response.status
+      end
+
+      # predicates
+      %w[error? client_error? server_error? connection_error?
+         redirect? html? image? javascript? css?
+         flash? video? redirected_to
+        ].each do |meth|
+        it "should include :#{meth}" do
+          entry.should respond_to(meth)
+          entry.send(meth).should == entry.response.send(meth)
+        end
+      end
+
+      # methods that take arguments
+      [:content_type?, :has_content?, :have_content?,
+       :match_content?, :matches_content?].each do |meth|
+        it "should include #{meth}" do
+          entry.should respond_to(meth)
+          entry.send(meth,'video').should == entry.response.send(meth,'video')
+        end
+      end
+
+    end
+
+    context "delegated methods from request" do
+      it "should include :url" do
+        entry.should respond_to(:url)
+        entry.url.should == entry.request.url
+      end
+    end
+
   end # Entry
 end # HAR
